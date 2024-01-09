3a4410dceda66a42115dd4ba46fd86815a699e47
hayabusabusa <hayabusabusaSY@gmail.com>
2024-01-09 21:31:10 +0900

    feat: ギャラリー画面の処理を実装
    Auth で認証を行なって Firestore のデータを取得する部分まで実装。

--- Package/Sources/GalleryFeature/GalleryView.swift
+++ Package/Sources/GalleryFeature/GalleryView.swift
@@ -5,18 +5,162 @@
 //  Created by Shunya Yamada on 2024/01/09.
 //
 
+import AuthClient
+import FirestoreClient
+import ComposableArchitecture
+import SharedModels
 import SwiftUI
 
-public struct GalleryView: View {
-    public var body: some View {
-        Text("Gallery")
+// MARK: - Reducer
+
+@Reducer
+public struct GalleryFeature {
+    public struct State: Equatable {
+        public var books = IdentifiedArrayOf<Book>()
+        public var isLoading = false
     }
 
-    public init() {}
+    public enum Action {
+        /// 画面に必要な情報が全て返ってきた時の `Action`.
+        case allResponse(Result<[Book], Error>)
+        /// 認証済みかどうか確認する `Action`.
+        case auth(Bool)
+        /// 匿名認証を行う `Action`.
+        case signIn
+        /// 匿名認証の結果が返ってきた時の `Action`.
+        case signInResponse(Result<User, Error>)
+        /// 非同期処理を実行するための `Action`.
+        case task
+    }
+
+    @Dependency(\.authClient) var authClient
+    @Dependency(\.date) var dateGenerator
+    @Dependency(\.firestoreClient) var firestoreClient
+
+    public var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case let .allResponse(.success(books)):
+                state.books = IdentifiedArray(uniqueElements: books)
+                state.isLoading = false
+
+                return .none
+            case .allResponse(.failure):
+                state.isLoading = false
+
+                return .none
+            case let .auth(isSignIn):
+                if isSignIn {
+                    return .run { send in
+                        await send(
+                            .allResponse(
+                                Result {
+                                    try await self.firestoreClient.fetchLatestBooks(
+                                        FirestoreClient.LatestBooksRequest(
+                                            orderBy: "createdAt",
+                                            isDescending: true,
+                                            afterDate: dateGenerator.now,
+                                            limit: 10
+                                        )
+                                    )
+                                }
+                            )
+                        )
+                    }
+                } else {
+                    return .send(.signIn)
+                }
+            case .signIn:
+                return .run { send in
+                    await send(
+                        .signInResponse(
+                            Result {
+                                try await self.authClient.signInAsAnonymousUser()
+                            }
+                        )
+                    )
+                }
+            case .signInResponse(.success):
+                return .run { send in
+                    await send(
+                        .allResponse(
+                            Result {
+                                try await self.firestoreClient.fetchLatestBooks(
+                                    FirestoreClient.LatestBooksRequest(
+                                        orderBy: "createdAt",
+                                        isDescending: true,
+                                        afterDate: dateGenerator.now,
+                                        limit: 10
+                                    )
+                                )
+                            }
+                        )
+                    )
+                }
+            case let .signInResponse(.failure(error)):
+                print(error)
+
+                return .none
+            case .task:
+                state.isLoading = true
+
+                return .send(.auth(authClient.isSignIn()))
+            }
+        }
+    }
+}
+
+// MARK: - View
+
+public struct GalleryView: View {
+    let store: StoreOf<GalleryFeature>
+
+    public var body: some View {
+        WithViewStore(store, observe: { $0 }) { viewStore in
+            NavigationStack {
+                if viewStore.isLoading {
+                    ProgressView()
+                } else {
+                    List {
+                        ForEach(viewStore.books) { book in
+                            Text(book.title)
+                        }
+                    }
+                }
+            }
+            .task {
+                store.send(.task)
+            }
+        }
+    }
+
+    public init(store: StoreOf<GalleryFeature>) {
+        self.store = store
+    }
 }
 
 #if DEBUG
 #Preview {
-    GalleryView()
+    GalleryView(
+        store: Store(initialState: GalleryFeature.State()) {
+            GalleryFeature()
+        } withDependencies: {
+            $0.authClient.isSignIn = {
+                true
+            }
+            $0.firestoreClient.fetchLatestBooks = { _ in
+                [
+                    .init(id: UUID().uuidString,
+                          title: "TEST",
+                          url: "",
+                          createdAt: Date(),
+                          imageURLs: [],
+                          categories: [],
+                          author: nil,
+                          thumbnailURL: nil)
+                ]
+            }
+        }
+    )
 }
 #endif
